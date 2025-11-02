c928bead182f6cc1957f6d4c7526193fb253a5ed
efondo-aaronkyle <akyleefondo@gmail.com>
2025-11-02 19:30:34 +0800

    add tailwind configuration

---
+++ assets/js/tailwind.config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        'pup-red': '#b22222',
+        'pup-yellow': '#ffd700',
+        'pup-white': '#ffffff',
+      },
+      fontFamily: {
+        'heading': ['Poppins', 'sans-serif'],
+        'body': ['Inter', 'sans-serif'],
+      },
+    }
+  }
+}
